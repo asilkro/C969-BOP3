10359cde233f24145f6c912023befadf56bc8e0c
asilkro <asilkro@me.com>
2022-06-10 10:34:17 -0700

    updated with some more guidance on the Log

--- Models/Log.cs
+++ Models/Log.cs
@@ -9,25 +9,33 @@ namespace Silkroski_C969.Models
 {
     public class Log
     {
-
-        public static void MakeLogFile() // Create a file to write to.
+        public static string logpath = System.AppDomain.CurrentDomain.BaseDirectory + "AppLog.txt"; // PWD + a filename?
+        public static void LogMessage(string userId, bool connectionSucceeded) // Create a file to write to.
         {
-            string path = System.AppDomain.CurrentDomain.BaseDirectory + "AppLog.txt"; // PWD + a filename?
-
-            if (!File.Exists(path))
+            string message = null;
+            // Check if connectionsucceeded; if succeeded message = user logged in @ datetime
+            // if failed, user unable to login @ datetime
+            if (!File.Exists(logpath))
             {
                 
-                using (StreamWriter writer = File.CreateText(path))
+                using (StreamWriter writer = File.CreateText(logpath))
                 {
+                    writer.WriteLine(message + " at " + System.DateTime.UtcNow + ".\n");
                     writer.WriteLine("New log file created at: " + System.DateTime.UtcNow + ".\n");
                 }
             }
 
+            if (connectionSucceeded == true)
+            {
+                using (StreamWriter writer = File.AppendText(logpath))
+                {
+                    
+                }
+            }
+
+
+
         }
 
-        public void AddLogMessage()
-        {
-            //TODO
-        }
     }
 }
